c71ea487c82b301a8232ae909c122e5df1a9302a
Stian Le Roux <stianleroux@gmail.com>
2020-08-02 20:28:54 +0200

    Latest Version
    Added Readme and Infrastructure

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
-# .NET-CleanArchitecture
-Clean Architecture Solution Template
+# Clean Architecture Solution Template
+
+This is a solution template for creating a backend framework following the principles of Clean Architecture. Create a new project based on this template by clicking the above Use this template button or download the solution.
+
+What is [CQRS](https://docs.microsoft.com/en-us/azure/architecture/patterns/cqrs#:~:text=The%20Command%20and%20Query%20Responsibility,performance%2C%20scalability%2C%20and%20security.)
+
+## Technologies
+
+- .NET Core 3.1
+- Entity Framework Core 3.1
+- MediatR
+- AutoMapper
+- FluentValidation
+- NUnit, FluentAssertions, Moq
+
+## Getting Started
+
+- [ ] Install the latest .NET Core SDK
+
+Create a folder for your solution and copy the clean code solution into it.
+
+Run [RenameSolution.ps1](./RenameSolution.ps1) on the new solution folder to rename it to your project.
+
+## **Overview**
+
+### **Tests**
+
+This will contain all unit tests.
+
+### **Common**
+
+This will contain all entities, enums, exceptions, interfaces and types.
+
+### **Database**
+
+SQL database layer.
+
+### **Data Access**
+
+Logic specific to the database layer.
+
+### **Core**
+
+This layer contains all business logic. It is dependent on the core/domain layer, but has no dependencies on any other layer or project. This layer defines interfaces that are implemented by outside layers. For example, if the application need to access a notification service, a new interface would be added to application and an implementation would be created within infrastructure.
+
+### **Infrastructure**
+
+This layer contains classes for accessing external resources such as file systems, web services, SMTP, and so on. These classes should be based on interfaces defined within the application layer.
+
+### **Apis**
+
+This layer is for all Apis or other ASP.NET Core projects. This layer depends on both the Core and Infrastructure layers, however, the dependency on Infrastructure is only to support dependency injection. Therefore only Startup.cs should reference Infrastructure.
+
---
+++ RenameSolution.ps1
@@ -0,0 +1,68 @@
+cls 
+$path = Read-Host -Prompt 'Input your solution folder'
+cd $path 
+$oldstring = "CleanArchitecture"
+$newstring = Read-Host -Prompt 'Input your new project name'
+
+$files = Get-ChildItem -Path $path *.* -rec -file -exclude obj,debug,bin,*.msi,*.exe,*.dll,*.snk 
+
+$loopCounter = 1 
+
+foreach ($file in $files) 
+{
+    ##(-join [char[]](Get-Content $file.PSPath -Encoding Byte -TotalCount 2)) -eq 'ÿþ'
+    Write-Host "FileName: $($file.Fullname)" 
+    $fileContents = Get-Content $file.FullName 
+    $fileContentsUpdated = $fileContents -replace $oldstring, $newstring 
+
+    if ($filecontents -ne $null) 
+    {
+        #Write-Host "---BEFORE----" 
+        #Write-Host $fileContents 
+        #Write-Host "---AFTER----" 
+        #Write-Host $fileContentsUpdated 
+
+        if ($fileContents -ne $fileContentsUpdated) 
+        {
+            if ($fileContents[0..2] -eq 'ÿþ') 
+            {
+                Write-Host "Updating File $($file.Name) [Unicode] "
+                Set-Content -path $file.FullName -value $fileContentsUpdated -Encoding Unicode 
+            }
+            else 
+            {
+                Write-Host "Updating File $($file.Name) [NOT-Unicode] "
+                Set-Content -path $file.FullName -value $fileContentsUpdated ## not Unicode 
+            }
+        }
+
+        $fileNameUpdate = $file.Name -replace $oldstring, $newstring 
+        if ($file.Name -ne $fileNameUpdate) 
+        {
+           Write-Host "Rename $($file.FullName) to $fileNameUpdate" 
+           Rename-Item -Path $file.FullName -NewName $fileNameUpdate 
+        }
+
+        $loopCounter = $loopCounter + 1 
+        if ($loopCounter -gt 2) 
+          {
+             #exit 
+          }
+
+   }
+}
+
+// Repeat again, to rename folders 
+$files = Get-ChildItem -Path $path *.* -rec -directory -exclude obj,debug,bin
+
+foreach ($file in $files) 
+{
+
+        $fileNameUpdate = $file.Name -replace $oldstring, $newstring 
+        if ($file.Name -ne $fileNameUpdate) 
+        {
+           Write-Host "Rename $($file.FullName) to $fileNameUpdate" 
+           Rename-Item -Path $file.FullName -NewName $fileNameUpdate 
+        }
+
+}
