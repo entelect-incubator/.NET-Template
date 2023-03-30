d8daeaacf60a40b8edf267f473277b832aa1e7de
Stian <stianleroux@gmail.com>
2023-03-30 08:37:23 +0200

    Replace AutoMapper with Mapperly

---
+++ Common/Mappers/Mapper.cs
@@ -0,0 +1,9 @@
+﻿namespace Common.Mappers;
+
+using Riok.Mapperly.Abstractions;
+
+[Mapper]
+public static partial class Mapper
+{
+    ////public static partial SampleModel SampleToSampleModel(this Sample sample);
+}
--- Core/DependencyInjection.cs
+++ Core/DependencyInjection.cs
@@ -6,7 +6,6 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
-        services.AddAutoMapper(Assembly.GetExecutingAssembly());
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
         services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
